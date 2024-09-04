a3be92bc5ef375684c867e926e6c53e7ce537fa0
Dominik Pascuttini <dpascut@algebra.hr>
2024-09-04 19:15:22 +0200

    Formatter se odlucio upaliti

--- src/home.js
+++ src/home.js
@@ -2,14 +2,14 @@ var i = 0;
 var wrongText = "Budi izvrstan u onom što vidiš.";
 var correctText = " voliš.";
 var secondText = "Zaiskri!";
-var speed = 100; 
-var pauseDuration = 1000; 
+var speed = 100;
+var pauseDuration = 1000;
 
 function typeWriter(text, callback) {
   if (i < text.length) {
     document.getElementById("typewriter").innerHTML += text.charAt(i);
     i++;
-    setTimeout(function() { typeWriter(text, callback); }, speed);
+    setTimeout(function () { typeWriter(text, callback); }, speed);
   } else if (callback) {
     setTimeout(callback, pauseDuration);
   }
@@ -19,20 +19,20 @@ function deleteText(count, callback) {
   if (count > 0) {
     var currentText = document.getElementById("typewriter").innerHTML;
     document.getElementById("typewriter").innerHTML = currentText.slice(0, -1);
-    setTimeout(function() { deleteText(count - 1, callback); }, speed);
+    setTimeout(function () { deleteText(count - 1, callback); }, speed);
   } else if (callback) {
     setTimeout(callback, pauseDuration);
   }
 }
 
 function startTyping() {
-  
-  typeWriter(wrongText, function() {
-   
+
+  typeWriter(wrongText, function () {
+
     var deleteCount = wrongText.length - wrongText.indexOf("što") - "što".length;
-    deleteText(deleteCount, function() {
+    deleteText(deleteCount, function () {
       document.getElementById("typewriter").innerHTML += correctText;
-      setTimeout(function() {
+      setTimeout(function () {
         document.getElementById("typewriter").innerHTML += "<br><span class='blinker'></span><span class='orange-text'> " + secondText + "</span>";
       }, pauseDuration);
     });
